f8b4605101e469f193af7a6027b824216fddcafe
sarahhinkley <sarahhinkley@ucsb.edu>
2026-09-25 10:27:40 -0700

    Week 0 Discussion: Data Wrangling

---
+++ week0/global_dams.R
@@ -0,0 +1,6 @@
+packages <- c("here", "janitor", "tidyverse", "sf", "terra", "tmap", "spData", "spDataLarge", "geodata", "kableExtra", "viridisLite")
+installed_packages <- packages %in% rownames(installed.packages())
+
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
